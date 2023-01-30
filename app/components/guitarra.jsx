cae0be143cc2850a05492a9032f9e8dc3184fcc4
MDMS29 <mdmazo30@misena.edu.co>
2023-01-29 21:50:47 -0500

    Second Commit

--- app/components/guitarra.jsx
+++ app/components/guitarra.jsx
@@ -4,7 +4,7 @@ const Guitarra = ({ guitarra }) => {
     const { imagen, nombre, descripcion, precio, url } = guitarra
     return (
         <div className="guitarra">
-            <img src={imagen.data.attributes.formats.small.url} alt={`Guitarra ${nombre}`} />
+            <img src={imagen.data.attributes.url} alt={`Guitarra ${nombre}`} />
             <div className="contenido">
                 <h3>{nombre}</h3>
                 <p className="descripcion">{descripcion}</p>
